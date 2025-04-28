a62727c1cdb199ee99631d6e6a816bc1b687b531
Althaf A <althafmuhammed484@gmail.com>
2025-04-29 04:41:34 +0530

    Authentication Routes are completed

---
+++ server/routes/auth.route.js
@@ -0,0 +1,19 @@
+import express from "express";
+const router = express.Router();
+
+import {protectRoute} from "../middlewares/auth.middleware.js"
+import {login, signup, logout, updateProfile, checkAuth} from "../controllers/auth.controller.js"
+
+
+
+
+router.post("/signup",signup);
+router.post("/login",login);
+router.post("/logout",logout);
+
+router.put("/update-profile", protectRoute, updateProfile)
+
+router.get("/check", protectRoute, checkAuth);
+
+
+export default router;
