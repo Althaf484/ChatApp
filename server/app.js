4bbb8e5422222218db6fe7b7a3ef7e1da9185956
Althaf A <althafmuhammed484@gmail.com>
2025-04-29 05:31:52 +0530

    message rotes are completed

--- server/app.js
+++ server/app.js
@@ -7,6 +7,7 @@ const app = express();
 import { connectDB } from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js"
 
 dotenv.config();
 
@@ -16,6 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message",messageRoutes)
 
 app.listen(PORT, () => {
     console.log("server is running on PORT:" + PORT);
